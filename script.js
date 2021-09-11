e3bc77de898e0b4c1f118ac577036e5f923f31ec
rustamnew <1rustamnew1@gmail.com>
2021-09-11 15:42:36 +0300

    fix

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slider = new ChiefSlider('.slider', {
-    loop: false
+$(document).ready(function(){
+    let slidesToShow = 3
+
+    if (window.innerWidth <= 1024) {
+        slidesToShow = 2
+    }
+    if (window.innerWidth <= 768) {
+        slidesToShow = 1
+    }
+
+
+    $('.slider').slick({
+        slidesToShow,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: true,
     });
+    console.log('test')
 });
